e3f052000848f67c18fd2650a51904e6284760b7
Guido Munoz <gmunoz@c1r9s5.42madrid.com>
2024-06-20 18:59:06 +0200

    window works and sprite shows

---
+++ colors.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmunoz <gmunoz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 16:48:31 by gmunoz            #+#    #+#             */
+/*   Updated: 2024/06/20 16:48:40 by gmunoz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+
+# define COLORS_H
+
+/* Color codes for printf  */
+# define DEFAULT "\033[0;39m"
+# define GRAY "\033[0;90m"
+# define RED "\033[0;91m"
+# define GREEN "\033[0;92m"
+# define YELLOW "\033[0;93m"
+# define BLUE "\033[0;94m"
+# define MAGENTA "\033[0;95m"
+# define CYAN "\033[0;96m"
+# define WHITE "\033[0;97m"
+
+#endif
--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 /*   By: gmunoz <gmunoz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:48:40 by gmunoz            #+#    #+#             */
-/*   Updated: 2024/06/20 14:00:45 by gmunoz           ###   ########.fr       */
+/*   Updated: 2024/06/20 18:42:43 by gmunoz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct	s_data {
 	void	*img;
@@ -20,29 +22,104 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+	int		steps;
+	int		screen_size;
+}				t_vars;
+
+int	close(int keycode, t_vars *vars)
+{
+	if (keycode == 65307)
+	{
+        mlx_destroy_window(vars->mlx, vars->win);
+        exit(0);
+    }
+    return (0);
+}
+int close_window(t_vars *vars)
+{
+    mlx_destroy_window(vars->mlx, vars->win);
+    exit(0);
+}
+
+
+int	key_hook(int keycode, t_vars *vars)
+{
+	if (keycode == 119 || keycode == 97 || keycode == 115 || keycode == 100)
+	{
+		printf("number of steps = %d\n", vars->steps);
+		vars->steps++;
+	}
+	return (0);
+}
+
+int main(int argc, char const *argv[])
+{
+	t_data	img;
+	t_vars	vars;
+	char	*relative_path = "./char_trial.xpm";
+	int		img_width;
+	int		img_height;
+
+	vars.steps = 1;
+	(void)argc;
+	(void)argv;
+	vars.mlx = mlx_init();
+	if (!vars.mlx)
+	{
+	    printf("mlx_init failed\n");
+	    return 1;
+	}
+	
+	vars.win = mlx_new_window(vars.mlx, 3200, 1800, "Tudi Gaimu");
+	if (!vars.win)
+	{
+	    printf("mlx_new_window failed\n");
+	    return 1;
+	}
+	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+	mlx_hook(vars.win, 17, 1L<<0, close_window, &vars);
+	mlx_key_hook(vars.win, key_hook, &vars);
+	img.img = mlx_xpm_file_to_image(vars.mlx, relative_path, &img_width, &img_height);
+	if (!img.img)
+	{
+	    printf("mlx_xpm_file_to_image failed\n");
+	    return 1;
+	}
+
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	if (!img.addr)
+	{
+		printf("mlx_get_data_addr failed\n");
+	    return 1;
+	}
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 1600, 950);
+	mlx_loop(vars.mlx);
+	return 0;
+}
+
+/* 
+img.img = mlx_new_image(mlx, 1920, 1080);
+ft_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+
 void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
-}
+} */
 
-int main(int argc, char const *argv[])
+
+/*
+mlx_mouse_hook(vars.win, mouse_hook, &vars);
+
+int mouse_hook(int button, int x, int y, t_vars *vars)
 {
-	void *mlx;
-	void *mlx_win;
-	t_data img;
+	printf("Mouse button %d pressed at position %d, %d\n", button, x, y);
+	(void)vars;
+	return (0);
+} */
 
-	(void)argc;
-	(void)argv;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Tudi Gaimu");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	ft_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
-	return 0;
-}
