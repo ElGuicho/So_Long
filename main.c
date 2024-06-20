ffdcedb57576f74bd0427d70ec73127221f69c89
Guido Munoz <gmunoz@c1r9s5.42madrid.com>
2024-06-20 14:01:53 +0200

    starting pixel input

--- main.c
+++ main.c
@@ -6,23 +6,43 @@
 /*   By: gmunoz <gmunoz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:48:40 by gmunoz            #+#    #+#             */
-/*   Updated: 2024/06/19 18:45:22 by gmunoz           ###   ########.fr       */
+/*   Updated: 2024/06/20 14:00:45 by gmunoz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int main(int argc, char const *argv[])
 {
 	void *mlx;
-	void *win;
+	void *mlx_win;
+	t_data img;
 
 	(void)argc;
 	(void)argv;
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 200, 200, "Ventanita");
-	(void)win;
-
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Tudi Gaimu");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	ft_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return 0;
 }
